75b0c04104954a012419c736186273f62029a051
xdeng7 <xdeng7@ucmerced.edu>
2021-02-24 20:16:12 -0800

    update modeling

---
+++ modeling/backbone/__init__.py
@@ -0,0 +1,15 @@
+from modeling.backbone import resnet, resnet_multiscale, xception, drn, mobilenet
+
+def build_backbone(backbone, output_stride, BatchNorm):
+    if backbone == 'resnet':
+        return resnet.ResNet101(output_stride, BatchNorm)
+    elif backbone == 'resnet_multiscale':
+        return resnet_multiscale.ResNet101(output_stride, BatchNorm)
+    elif backbone == 'xception':
+        return xception.AlignedXception(output_stride, BatchNorm)
+    elif backbone == 'drn':
+        return drn.drn_d_54(BatchNorm)
+    elif backbone == 'mobilenet':
+        return mobilenet.MobileNetV2(output_stride, BatchNorm)
+    else:
+        raise NotImplementedError
